c9d53af734541dd3eab16dbc4e207c3655fa9c87
Ubuntu <ubuntu@ip-172-31-47-45.ap-northeast-2.compute.internal>
2020-08-05 07:47:39 +0000

    day3_nodejs_글목록출력하기

--- nodejs_study/main.js
+++ nodejs_study/main.js
@@ -6,36 +6,80 @@ var url = require('url');
 var app = http.createServer(function(request,response){
 	    var _url = request.url;
 		var queryData = url.parse(_url, true).query;
-		var title = queryData.id;
-		console.log(queryData.id);//?id=
-		if(_url == '/'){
-			      title='Welcome';
+		var pathname = url.parse(_url, true).pathname;
+
+		// console.log(url.parse(_url,true));확인해보면 pathname은 /이다 
+	
+		if(pathname === '/'){ // 정상적인 페이지를요청하면
+			if(queryData.id === undefined){ // home 일때
+				fs.readdir('./data', function(error, filelist){ //file list 가져오기
+					console.log(filelist);
+					var title = 'Welcome';
+					var description = 'Hello, Node.js';
+					//여기서 정의한 title과 description이 밑의 html안의 변수에 들어간다.
+					var list = '<ul>';
+					var i = 0;
+					while(i < filelist.length){ //파일 갯수 만큼
+						list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+						i++;
+					}
+
+					list += '</ul>';
+				
+					var template = `
+						<html>
+						<head>
+  						<title>${title}</title>
+    					<meta charset="utf-8">
+						</head>
+						<body>
+		  					<h1><a href="/">WEB</a></h1>
+						 	${list}
+							<h2>${title}</h2>
+							 <p>${description}</p>
+						</body>
+						</html>`;
+					response.writeHead(200);//정상적인 페이지
+					response.end(template);
+				});
+			}
+			else{
+				fs.readdir('./data', function(error, filelist){
+					console.log(filelist);
+					var title = 'Welcome';
+					var description = 'Hello, Node.js';
+					var list = '<ul>';
+					var i = 0;
+					while(i < filelist.length){
+						list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+						i++;
+					}
+
+					list += '</ul>';
+					fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+						var title = queryData.id;
+						var template = `
+							<html>
+							<head>
+								<title>WEB1 - ${title}</title>
+								<meta charset="utf-8">
+							</head>
+							<body>
+								<h1><a href="/">WEB</a></h1>
+								${list}
+								<h2>${title}</h2>
+								<p>${description}</p>
+							</body>
+							<html>`;
+						response.writeHead(200);
+						response.end(template);
+					});
+				});
+			}	
 		}
-		if(_url == '/favicon.ico'){
-				  response.writeHead(404);
-				  response.end();
-				  return;
+		else{
+			response.writeHead(404);
+			response.end('Not found');
 		}
-		response.writeHead(200);
-		fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-		var template = `
-			<html>
-			<head>
-  			<title>${title}</title>
-    		<meta charset="utf-8">
-			</head>
-			<body>
-	  			<h1><a href="/">WEB</a></h1>
-	    		<ol>
-		    		<li><a href="/?id=HTML">HTML</a></li>
-			    	<li><a href="/?id=CSS">CSS</a></li>
-				    <li><a href="/?id=JavaScript">JavaScript</a></li>
-				 </ol>
-				 <h2>${title}</h2>
-				 <p>${description}</p>
-			</body>
-			</html>`;
-			response.end(template);
-		})
 });
 app.listen(3000);
